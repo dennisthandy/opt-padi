137964cf2aa10efbb65867a60addef1adc8de8f1
Dennis Thandy <dennisthandy15@gmail.com>
2021-07-07 08:31:16 +0700

    add dataset download

--- mypajale/helper/identify.py
+++ mypajale/helper/identify.py
@@ -7,19 +7,25 @@ from helper.model import load_cnn_model, load_lvq_model
 from helper.lvq import predict
 
 labels = ['Brown Spot', 'Healthy', 'Hispa', 'Leaf Blast']
+detail_labels = {
+    0: 'Penyakit bercak daun coklat (brown leaf spot) pada tanaman padi (oryza sativa l) ini disebabkan oleh cendawan Helminthosporium oryzae atau Drechslera oryzae (Cochliobolus miyabeanus).',
+    1: 'Kondisi daun berwarna hijau terang tanpa ada bercak.',
+    2: 'Kerusakan disebabkan oleh serangga dewasa dan larva hispa padi, Dicladispa armigera. Serangga dewasa mengikis permukaan atas helai daun dan hanya menyisakan epidermis bawah.',
+    3: 'Penyakit blas yang disebabkan oleh jamur Pyricularia oryzae Cav. [sinonim Magnaportheoryzae (Hebert) Barr] merupakan salah satu penyakit penting pada tanaman padi di seluruh dunia'
+}
 
 def cnn_predict(data):
     model = load_cnn_model()
     result = np.argmax(model.predict(data))
 
-    return labels[result]
+    return { 'name': labels[result], 'desc': detail_labels[result] }
 
 
 def lvq_predict(data):
     model = load_lvq_model()
     result = predict(data, model)
 
-    return labels[result[0]]
+    return { 'name': labels[result[0]], 'desc': detail_labels[result[0]] }
 
 
 '''model = tf.keras.models.load_model(os.path.join(settings.BASE_DIR, ('static/model.h5')))
--- static/js/home.js
+++ static/js/home.js
@@ -6,6 +6,7 @@ const labelFileContent = labelFile.innerHTML;
 const formSubmit = document.querySelector('.form__submit');
 const loading = document.querySelector('.loading');
 const result = document.querySelector('.result');
+const download = document.querySelectorAll('.download')
 
 // home
 inputFile.addEventListener('change', (e) => {
@@ -20,4 +21,26 @@ inputFile.addEventListener('change', (e) => {
 form.addEventListener('submit', (e) => {
     loading.classList.remove('hidden');
     result.classList.add('hidden');
+})
+
+function downloadData(label){
+    var link = document.createElement('a');
+
+    link.setAttribute('download', null);
+    link.style.display = 'none';
+
+    document.body.appendChild(link);
+
+    for (var i = 0; i < 4; i++) {
+        link.setAttribute('href', `/static/img/data/${label}/${label}_${i + 1}.jpg`);
+        link.click();
+    }
+
+    document.body.removeChild(link);
+}
+
+download.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        downloadData(element.id)
+    })
 })
